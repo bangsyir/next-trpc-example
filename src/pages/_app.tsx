007c32546404df8b4f15f28f00ded6ed0bb3d3e1
bangsyir <bangsyir@gmail.com>
2023-08-24 18:04:18 +0800

    add trpc and SessionProvider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,17 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import { trpc } from "@/utils/trpc";
+import { Session } from "next-auth";
+import { SessionProvider } from "next-auth/react";
+import type { AppProps, AppType } from "next/app";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
+const App: AppType<{ session: Session | null }> = ({
+  Component,
+  pageProps: { session, ...pageProps },
+}: AppProps) => {
+  return (
+    <SessionProvider session={session}>
+      <Component {...pageProps} />
+    </SessionProvider>
+  );
+};
+export default trpc.withTRPC(App);
